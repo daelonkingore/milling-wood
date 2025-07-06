38a6ceeb83e301354f3a9b95058497599cd9629e
daelonkingore <daelonkingore@gmail.com>
2025-07-06 17:10:04 -0500

    added pics and updated carousel

--- src/components/Description.vue
+++ src/components/Description.vue
@@ -18,8 +18,15 @@ export default {
     <div>
         <h3>Who we are</h3>
         <div>
-            <img :src="imageSrc" @error="setPlaceholder" alt="Image" />
+          <img src="../assets/people/group_pic1.jpg" alt="Example Image" />
         </div>
         <span>We are a family run milling business</span>
     </div>
 </template>
+
+<style scoped>
+img {
+  border-radius: 8px;
+  width: 75%;
+}
+</style>

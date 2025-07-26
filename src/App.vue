bbb0bcb78879e70dcd28ca1d033341419bd3f526
daelonkingore <daelonkingore@gmail.com>
2025-07-26 13:11:16 -0500

    nav and services chgs added vuetify

--- src/App.vue
+++ src/App.vue
@@ -8,35 +8,47 @@ import Contacts from './components/Contacts.vue'
 </script>
 
 <template>
-  <div>
-    <Navigation></Navigation>
+  <v-app>
+    <div>
+      <Navigation id="top"></Navigation>
 
-    <h1>Milling By Darrell</h1>
+      <h1>Milling By Darrell</h1>
 
-    <div class="bottom-spacing">
-      <CarouselElem></CarouselElem>
-    </div>
+      <div class="bottom-spacing">
+        <CarouselElem></CarouselElem>
+      </div>
 
-    <div class="bottom-spacing">
-    <Services></Services>
-    </div>
-    
-    <div class="bottom-spacing">
-      <Wood></Wood>
-    </div>
-    
-    <div class="bottom-spacing">
-    <Description></Description>
-    </div>
+      <div id="services" class="bottom-spacing">
+      <Services></Services>
+      </div>
+      
+      <div id="wood" class="bottom-spacing">
+        <Wood></Wood>
+      </div>
+      
+      <div id="desc" class="bottom-spacing">
+      <Description></Description>
+      </div>
 
-    <div class="bottom-spacing">
-      <Contacts></Contacts>
+      <div id="contacts" class="bottom-spacing">
+        <Contacts></Contacts>
+      </div>
+      <a id="top-link" href="#top">Back to Top</a>
     </div>
-  </div>
+  </v-app>
 </template>
 
 <style scoped>
 .bottom-spacing {
   margin-bottom: 100px;
+}
+
+#top-link {
+  color: #ddd;
+}
+
+#top-link:hover {
+  color: #ddd;
+  background-color: #5b542d;
 }
 </style>
--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,6 +1,11 @@
 <template>
-    <div id="nav">
-        <span>Navigation goes here</span>
+    <div>
+        <nav id="nav">
+            <a href="#services">Services</a>
+            <a href="#wood">Wood</a>
+            <a href="#desc">Who we Are</a>
+            <a href="#contacts">Contact Us</a>
+        </nav>
     </div>
 </template>
 
@@ -9,7 +14,21 @@
     background-color: darkgreen;
     height: 35px;
     display: flex;
-    justify-content: center; /* Centers horizontally */
+    justify-content: center;
     align-items: center;
+    cursor: pointer;
+}
+
+#nav > * {
+    padding: 0 15px;
+    padding-top: 7px;
+    color: #ddd;
+    height: 100%;
+    transition: background-color 0.3s ease;
+}
+
+#nav > *:hover {
+    transition: background-color 0.3s ease;
+    background-color: #5e462a;
 }
 </style>

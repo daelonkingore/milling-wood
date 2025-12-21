d2ea58ab01dd5ff70819d3351657e2c065ed9039
daelonkingore <daelonkingore@gmail.com>
2025-12-20 20:22:32 -0600

    add vuetify plugin in config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vuetify from 'vite-plugin-vuetify';
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/milling-wood/',
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    vuetify()
+  ],
 })

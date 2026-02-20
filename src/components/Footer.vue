76e58edf6e5b19bcbfd6614a61b684b19ca2c3c0
daelonkingore <daelonkingore@gmail.com>
2026-02-20 00:22:42 -0600

    update to new carousel

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,15 @@
 <template>
-    <v-footer class="text-center" padless>
-        <v-col class="text-center" cols="12">
+    <v-footer padless>
+        <v-col cols="12">
             <strong>2025 — Milling By Darrell, LLC</strong>
+            <br><div class="small-words-footer">Website created by Daelon Kingore</div>
         </v-col>
     </v-footer>
 </template>
+
+<style scoped>
+.small-words-footer {
+    font-size: 10px;
+    color: #a8a6a6;
+}
+</style>
